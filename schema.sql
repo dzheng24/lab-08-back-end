60b2d9ee8ffe25539b354c2eac34c4653bee97c2
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-09-23 08:57:23 -0700

    added table in database

--- schema.sql
+++ schema.sql
@@ -1,17 +1,20 @@
-DROP DATABASE IF EXISTS city_explorer;
-
-CREATE DATABASE city_explorer;
-
-\c city_explorer
-
 DROP TABLE IF EXISTS locationtable;
+DROP TABLE IF EXISTS weather;
 
 CREATE TABLE locationtable(
     id SERIAL PRIMARY KEY,
     search_query VARCHAR(255),
     formatted_query VARCHAR(255),
-    latitude VARCHAR(255),
-    longitude VARCHAR(255),
+    latitude NUMERIC(8,6),
+    longitude NUMERIC(9,6)
+);
+
+CREATE TABLE weather(
+    id SERIAL PRIMARY KEY,
+    forecast VARCHAR(255),
+    time VARCHAR(255),
+    location_id INTEGER NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES locationtable (id)
 );
 
 
